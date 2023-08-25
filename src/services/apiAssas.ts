e2009058c2d53cacb9de788e50b0c2f45c9e984b
Leticia Rosae <leticiarosadeoliveira@yahoo.com.br>
2023-08-25 20:26:25 -0300

    Building

--- src/services/apiAssas.ts
+++ src/services/apiAssas.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5050',
+  baseURL: import.meta.env.VITE_REACT_APP_API_ASSAS_BASE_URL,
   headers: {
     accept: 'application/json',
     'content-type': 'application/json',
